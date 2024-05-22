069944d480d5d2d8e2d25cfdc771357f58b9cd0a
Hasina7124 <raharison.m@zurcher.edu.mg>
2024-05-22 22:49:39 +0300

    1th commit

--- Candidature/Mere.cs
+++ Candidature/Mere.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Candidature
 {
-    public partial class Form1 : Form
+    public partial class Mere : Form
     {
-        public Form1()
+        public Mere()
         {
             InitializeComponent();
         }
