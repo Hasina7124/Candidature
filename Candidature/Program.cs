38f5ea9655795ba625127ffcaf7d1963b5caa5ed
Hasina7124 <raharison.m@zurcher.edu.mg>
2024-05-23 17:30:14 +0300

    1st commit

--- Candidature/Program.cs
+++ Candidature/Program.cs
@@ -16,7 +16,7 @@ namespace Candidature
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new InsertionCandidat());
+            Application.Run(new Mere());
         }
     }
 }
